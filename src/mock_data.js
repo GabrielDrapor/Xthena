1109915cc113ee5aaa4255775b9452e50c74c3ab
Drapor <gabrieldrapor@gmail.com>
2021-08-03 00:22:00 +0800

    base interface

---
+++ src/mock_data.js
@@ -0,0 +1,219 @@
+const maps = [
+  {
+    "hint": "哈利·波特第一个有好感的女生。",
+    "direction": "h",
+    "answer": "秋张",
+    "start_coor": [
+      0,
+      2
+    ]
+  },
+  {
+    "hint": "我国最严厉的外交辞令。",
+    "direction": "h",
+    "answer": "勿谓言之不预也",
+    "start_coor": [
+      2,
+      0
+    ]
+  },
+  {
+    "hint": "由新丽传媒出品，由汪俊执导，流潋紫担任编剧，周迅、霍建华领衔主演的古装电视剧。",
+    "direction": "h",
+    "answer": "如懿传",
+    "start_coor": [
+      1,
+      6
+    ]
+  },
+  {
+    "hint": "一个网络脱口秀节目，主持人戴着标志性头套。",
+    "direction": "h",
+    "answer": "暴走大事件",
+    "start_coor": [
+      9,
+      5
+    ]
+  },
+  {
+    "hint": "本名李嘉琦的网络短视频up主。",
+    "direction": "h",
+    "answer": "辣目洋子",
+    "start_coor": [
+      9,
+      0
+    ]
+  },
+  {
+    "hint": "《火影忍者》中大蛇丸的部下，后来修得仙法。",
+    "direction": "h",
+    "answer": "药师兜",
+    "start_coor": [
+      7,
+      4
+    ]
+  },
+  {
+    "hint": "洗耳恭听一词的主人公",
+    "direction": "h",
+    "answer": "许由",
+    "start_coor": [
+      5,
+      1
+    ]
+  },
+  {
+    "hint": "一款外卖APP",
+    "direction": "h",
+    "answer": "回家吃饭",
+    "start_coor": [
+      4,
+      4
+    ]
+  },
+  {
+    "hint": "周杰伦《Jay》中的一首歌。",
+    "direction": "h",
+    "answer": "星晴",
+    "start_coor": [
+      6,
+      0
+    ]
+  },
+  {
+    "hint": "米哈游制作的游戏。",
+    "direction": "h",
+    "answer": "原神",
+    "start_coor": [
+      8,
+      3
+    ]
+  },
+  {
+    "hint": "柏拉图著作之一。",
+    "direction": "v",
+    "answer": "理想国",
+    "start_coor": [
+      5,
+      8
+    ]
+  },
+  {
+    "hint": "《让子弹飞》的主人公。",
+    "direction": "v",
+    "answer": "张牧之",
+    "start_coor": [
+      0,
+      3
+    ]
+  },
+  {
+    "hint": "言论自由",
+    "direction": "v",
+    "answer": "言论自由",
+    "start_coor": [
+      2,
+      2
+    ]
+  },
+  {
+    "hint": "不喜勿喷",
+    "direction": "v",
+    "answer": "不喜勿喷",
+    "start_coor": [
+      0,
+      0
+    ]
+  },
+  {
+    "hint": "传播方式",
+    "direction": "v",
+    "answer": "人传人",
+    "start_coor": [
+      0,
+      8
+    ]
+  },
+  {
+    "hint": "代餐产品",
+    "direction": "v",
+    "answer": "若饭",
+    "start_coor": [
+      3,
+      7
+    ]
+  },
+  {
+    "hint": "围棋棋局进行中估算双方的势力范围的行为。",
+    "direction": "v",
+    "answer": "点目",
+    "start_coor": [
+      8,
+      1
+    ]
+  },
+  {
+    "hint": "哲学",
+    "direction": "v",
+    "answer": "哲学",
+    "start_coor": [
+      4,
+      9
+    ]
+  },
+  {
+    "hint": "明代思想",
+    "direction": "h",
+    "answer": "理学",
+    "start_coor": [
+      5,
+      8
+    ]
+  },
+  {
+    "hint": "回文",
+    "direction": "v",
+    "answer": "回文",
+    "start_coor": [
+      4,
+      4
+    ]
+  },
+  {
+    "hint": "中间件",
+    "direction": "v",
+    "answer": "中间件",
+    "start_coor": [
+      7,
+      9
+    ]
+  },
+  {
+    "hint": "俗语",
+    "direction": "v",
+    "answer": "吃不了兜着走",
+    "start_coor": [
+      4,
+      6
+    ]
+  },
+  {
+    "hint": "陈粒的一首歌。",
+    "direction": "v",
+    "answer": "如也",
+    "start_coor": [
+      1,
+      6
+    ]
+  },
+  {
+    "hint": "台湾对于「高中」的说法。",
+    "direction": "h",
+    "answer": "国中",
+    "start_coor": [
+      7,
+      8
+    ]
+  }
+];
+export { maps };
