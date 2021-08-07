000996cac9b65cb3568e6788e8d0be36aa31b100
Drapor <gabrieldrapor@gmail.com>
2021-08-07 23:18:28 +0800

    add CORS

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,14 @@ import os
 import json
 
 from flask import Flask
+from flask_cors import CORS
 from deta import Deta
 
 deta = Deta(os.environ["DETA_AK"])
 puzzles = deta.Base("puzzles")
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/get-puzzle", methods=["GET"])
