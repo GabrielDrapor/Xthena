b576482ed2a9891084585f129a7547f971547563
envl <yyaomingm@gmail.com>
2021-08-08 16:21:51 +0800

    refactor onInput

--- frontend/index.js
+++ frontend/index.js
@@ -49,30 +49,29 @@ function genAnswerTable(questions) {
 
 function onInput(evt) {
   if (/[\u4e00-\u9fa5]+/.test(evt.data)) {
-    const [x, y] = getXY(evt.target)
-    var direction = get_select_direction()
-    for (var i = 0; i < evt.data.length; i++) {
-      if (
-        (direction == 'h') & (y + i >= 10) ||
-        (direction == 'v') & (x + i >= 10)
-      ) {
-        return
-      }
-      var cell =
-        direction == 'h' ? get_td_by_coor(x, y + i) : get_td_by_coor(x + i, y)
-      if (cell.classList.contains('empty')) {
-        return
-      }
-      const input = cell.getElementsByTagName('input')[0]
-      input.value = evt.data[i]
-      check_answer(input)
-    }
+    const [x0, y0] = getXY(evt.target)
+    const dir = evt.target['data-vh']
+    Array.from(elTable.querySelectorAll('.selected > input'))
+      .filter(el => {
+        const [x, y] = getXY(el)
+        if (dir === 'h') {
+          return x >= x0
+        } else {
+          return y >= y0
+        }
+      })
+      .forEach((input, i) => {
+        if (!evt.data[i]) return
+        input.value = evt.data[i]
+        check_answer(input)
+      })
   }
 }
 
 function getAnswer(x, y) {
   return answerTable[x][y].length ? answerTable[x][y][0].char : null
 }
+
 function check_answer(input) {
   const [x, y] = input.getAttribute('data-xy').split(',')
   const ans = getAnswer(x, y)
@@ -88,11 +87,6 @@ function get_td_by_coor(x, y) {
   return document.querySelector(`td[data-xy="${x},${y}"]`)
 }
 
-function get_select_direction() {
-  var selected_cells = document.getElementsByClassName('selected')
-  return selected_cells[0].cellIndex == selected_cells[1].cellIndex ? 'v' : 'h'
-}
-
 // return [x,y]
 function getXY(el) {
   return el.getAttribute('data-xy').split(',')
@@ -106,32 +100,29 @@ function cell_click(event) {
 
   const [x, y] = getXY(clickedInput)
   const ansObjs = answerTable[x][y]
-
   let activeQuestion
 
   document
     .querySelectorAll('.selected')
     .forEach(el => el.classList.remove('selected'))
 
-  if (ansObjs.length === 1) {
-    activeQuestion = ansObjs[0].question
-  }
-  // len == 2
-  else {
+  if (ansObjs.length === 2) {
     if (clickedInput['data-vh'] === 'h') clickedInput['data-vh'] = 'v'
     else clickedInput['data-vh'] = 'h'
-
     activeQuestion = ansObjs.filter(
       obj => obj.question.direction === clickedInput['data-vh']
     )[0].question
+  } else {
+    activeQuestion = ansObjs[0].question
   }
-  console.log({ activeQuestion })
+
   getQuestionCoords(activeQuestion).forEach(({ x, y }) => {
     const td = get_td_by_coor(x, y)
     td.classList.add('selected')
     const input = td.querySelector('input')
     input && (input['data-vh'] = clickedInput['data-vh'])
   })
+
   elHint.innerText = activeQuestion.hint
 }
 
